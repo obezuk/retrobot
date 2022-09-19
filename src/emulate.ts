4e74c5473051fc975473170121bfcf5c5681ba29
Tim Obezuk <tim@obezuk.com>
2022-09-19 01:07:56 +0000

    POC: Encoding gif directly without ffmpeg

--- src/emulate.ts
+++ src/emulate.ts
@@ -6,20 +6,17 @@ import Piscina from 'piscina';
 import encode from 'image-encode';
 import { crc32 } from 'hash-wasm';
 import * as shelljs from 'shelljs';
-import ffmpeg from 'fluent-ffmpeg';
 import { performance } from 'perf_hooks';
 import { values, first, size, last, isEqual } from 'lodash';
-import { path as ffmpegPath } from '@ffmpeg-installer/ffmpeg';
-import { path as ffprobePath } from '@ffprobe-installer/ffprobe';
 
 import { arraysEqual, InputState, isDirection, rgb565toRaw } from './util';
 import { emulateParallel } from './workerInterface';
 import { Frame } from './worker';
 
-tmp.setGracefulCleanup();
+import sharp from 'sharp';
+import * as GIF from 'sharp-gif2';
 
-ffmpeg.setFfmpegPath(ffmpegPath);
-ffmpeg.setFfprobePath(ffprobePath);
+tmp.setGracefulCleanup();
 
 const RECORDING_FRAMERATE = 30;
 
@@ -118,6 +115,7 @@ export const emulate = async (pool: Piscina, coreType: CoreType, game: Uint8Arra
     const startFrames = performance.now();
 
     const { frames } = data;
+
     const importantFrames: (Frame & { renderTime: number })[] = [];
     let lastFrame: Frame;
     let durationSinceFrame = 0;
@@ -146,101 +144,34 @@ export const emulate = async (pool: Piscina, coreType: CoreType, game: Uint8Arra
         })
     }
 
-    const tmpFrameDir = tmp.dirSync({ unsafeCleanup: true });
-
     const { width, height } = last(importantFrames);
 
-    const images = await Promise.all(importantFrames.map((frame) => {
-        const file = path.join(tmpFrameDir.name, `frame-${frame.renderTime}.bmp`);
+    var tmpGif = GIF.createGif({
+        'width': width * 2,
+        'height': height * 2,
+        'delay' : 1,
+        'repeat' : 1
+    })
 
-        return new Promise<{ file: string, frameNumber: number }>((res, rej) =>
-            fs.writeFile(file, Buffer.from(encode(rgb565toRaw(frame), [width, height],'bmp')), (err) => {
-                if (err) {
-                    rej(err)
-                } else {
-                    res({
-                        file,
-                        frameNumber: frame.renderTime
-                    });
-                }
-            }));
-    }))
+    for (var f in importantFrames) {
+        var buffer = Buffer.from(encode(rgb565toRaw(importantFrames[f]), [width, height],'png'));
+        var sharpInstance = sharp(buffer);
+        tmpGif.addFrame(sharpInstance)
+    }
 
     const endFrames = performance.now();
     console.log(`Exporting frames: ${endFrames - startFrames}`);
 
     const startEncode = performance.now();
 
-    let framesTxt = '';
-    for (let i = 0; i < images.length; i++) {
-        const current = images[i];
-
-        framesTxt += `file '${current.file}'\n`;
-
-        const next = images[i + 1];
-        if (next) {
-            framesTxt += `duration ${(next.frameNumber - current.frameNumber) / 60}\n`;
-        }
-    }
-
-    framesTxt += `duration ${1 / 60}\n`;
-    framesTxt += `file '${last(images).file}'\n`;
-    framesTxt += `duration 5\n`;
-    framesTxt += `file '${last(images).file}'\n`;
-
-    const tmpFramesList = tmp.fileSync({ discardDescriptor: true });
-    fs.writeFileSync(tmpFramesList.name, framesTxt);
-
-    const { name: outputName } = tmp.fileSync();
-    const gifOutput = `${outputName}.gif`;
-    const mp4Output = `${outputName}.mp4`;
-    let output = gifOutput;
-
-    await new Promise<void>((res, rej) =>
-        ffmpeg()
-            .input(tmpFramesList.name)
-            .addInputOption('-safe', '0')
-            .inputFormat('concat')
-            .addOption('-filter_complex', `scale=2*iw:2*ih:flags=neighbor,split=2 [a][b]; [a] palettegen=reserve_transparent=off [pal]; [b] fifo [b]; [b] [pal] paletteuse=dither=bayer:bayer_scale=5`)
-            .output(gifOutput)
-            .on('error', (err, stdout, stderr) => {
-                console.log(stdout)
-                console.error(stderr);
-                rej(err)
-            })
-            .on('end', res)
-            .run());
-
-    if (fs.statSync(gifOutput).size > 8 * 1024 * 1024) {
-        output = mp4Output
-
-        await new Promise<void>((res, rej) =>
-            ffmpeg()
-                .input(gifOutput)
-                .output(mp4Output)
-                .on('error', (err, stdout, stderr) => {
-                    console.log(stdout)
-                    console.error(stderr);
-                    rej(err)
-                })
-                .on('end', res)
-                .run());
-    }
-
-    const recordingBuffer = fs.readFileSync(output);
-
-    shelljs.rm('-rf', gifOutput);
-    shelljs.rm('-rf', mp4Output);
-
-    tmpFrameDir.removeCallback();
-    tmpFramesList.removeCallback();
+    var tmpGifBuffer = Buffer.from(await tmpGif.toBuffer())
 
     const endEncode = performance.now();
     console.log(`Encoding: ${endEncode - startEncode}`);
 
     return {
         state: data.state,
-        recording: recordingBuffer,
-        recordingName: path.basename(output)
+        recording: tmpGifBuffer,
+        recordingName: 'event.gif'
     }
 }
